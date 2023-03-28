6dd31783ebc009eb73c3712a70fa532018c7f09e
Yonas-coder <52844663+Yonas-coder@users.noreply.github.com>
2023-03-28 14:11:01 -0400

    second commit

---
+++ Game2048/script.js
@@ -0,0 +1,33 @@
+// Game: 
+// at starting
+// generate a 4X4 array with all zerros 
+// put two 2s at random positions
+// show it to the user
+
+// check if any of the cells value is == 2048
+
+// wait for the user input [up, down, left, or right] and/or [w, s, a, d]
+
+// if w => move the 2s up {
+    // if the tiles with 2s are in the same column 
+    // if nothing other than 0s are in between the two tiles
+        // add the numbers (=4)
+        // put it all the way to the top of the calumn
+    // select a random cell with >0
+    // change the value to 2
+    // return the result 
+    // }
+
+// else if s => { ...
+    // 
+    // }
+// else if a => { ...
+    // 
+    // }
+// else if d => { ...
+    // 
+    // }
+// else => { no response}
+
+// wait for next valid key from user
+
